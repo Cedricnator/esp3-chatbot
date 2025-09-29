d91d7abd721c8d7e3a5daf20478ef0e4d381e084
cedricnator <Cedric.kirmayr@gmail.com>
2025-09-28 22:33:29 -0300

    feat[app]: add evaluation process
    Co-authored-by: Cucharoth <Cucharoth@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ from rag.re_ranker import CrossEncoderReranker
 from rag.prompts import build_synthesis_prompt
 from adapters.arg_adapter import ArgAdapter
 from adapters.arg_parser import ArgParser
+from eval.evaluate import Evaluator, GoldSet, EvaluatorAgent
 
 load_dotenv()
 
@@ -22,26 +23,43 @@ class Main:
         message = ""
         system_prompt = "You're a helpfull asistant"
         args = self.parser.parse()
+        
+        faiss_index_path = "data/processed/index.faiss"
+        chunks_path = "data/processed/chunks.parquet"
+        mapping_path = "data/processed/mapping.parquet"
 
+        retriever = FaissRetriever(faiss_index_path, chunks_path, mapping_path, self._logger)
+        reranker = CrossEncoderReranker(self._logger)
+        rag_orchestrator = RAGOrchestrator(retriever,self._logger, reranker)
+        
         if args.message:
             message = args.message
             
         if args.rag is not False:
             self._logger.info("running rag...")
-            faiss_index_path = "data/processed/index.faiss"
-            chunks_path = "data/processed/chunks.parquet"
-            mapping_path = "data/processed/mapping.parquet"
-
-            retriever = FaissRetriever(faiss_index_path, chunks_path, mapping_path, self._logger)
-            reranker = CrossEncoderReranker(self._logger)
-            rag_orchestrator = RAGOrchestrator(retriever,self._logger, reranker)
-
             query = message
             self._logger.info(f"Running RAG for query: {query}")
             result = rag_orchestrator.run(query, k_retrieve=5, rerank_top_n=5, do_rewrite=True)
             self._logger.info("RAG result:")
             self._logger.info(f"{result}")
             system_prompt =  build_synthesis_prompt(result['query'], result['hints']) # type: ignore
+        
+        if args.evaluation is not False:
+            self._logger.info("runnnig evaluation...")
+            gold_set =  GoldSet("./gold_set.json", self._logger)
+            deepseek_logger = LoggerStdin("deepseek_logger", "logs/deepseek.log")
+            d_provider = DeepSeekProvider(deepseek_logger)
+            evaluator_agent = EvaluatorAgent(self._logger, gold_set)
+            rag_orchestrator = RAGOrchestrator(retriever, self._logger, reranker) # type: ignore
+
+            evaluator = Evaluator(
+                gold_set,                
+                d_provider,
+                evaluator_agent,
+                rag_orchestrator,
+            )
+            evaluator.run()
+            return
 
         if args.provider:
             provider: str = args.provider
