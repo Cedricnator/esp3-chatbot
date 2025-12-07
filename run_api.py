d75bd440f1c1dbc7c3bef63667acbd39efc15da4
cedricnator <Cedric.kirmayr@gmail.com>
2025-12-07 15:11:14 -0300

    fix(run_api): change API port from 8000 to 33201

--- run_api.py
+++ run_api.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "src.api:app",
         host="0.0.0.0",
-        port=8000,
+        port=33201,
         reload=True,  # Auto-reload en desarrollo
         log_level="info"
     )
