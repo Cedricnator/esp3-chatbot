3e9a7a1f24e346fda677d7221c1abaad54d3dc3c
cedricnator <Cedric.kirmayr@gmail.com>
2025-12-07 15:23:09 -0300

    fix(run_api): update API port from 33201 to 33209

--- run_api.py
+++ run_api.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "src.api:app",
         host="0.0.0.0",
-        port=33201,
+        port=33209,
         reload=True,  # Auto-reload en desarrollo
         log_level="info"
     )
